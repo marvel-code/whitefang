eb022525487fdd72999c89c0c844c9088f4da505
Rodion Kostin <itrodion@gmail.com>
2022-08-22 18:40:12 +0300

    slider ok

--- app/js/indexSlider.js
+++ app/js/indexSlider.js
@@ -1,33 +1,35 @@
 import { Swiper } from 'swiper'
 import $ from 'jquery';
 
-const SLIDE_STOP_TIME_MS = 1000;
+const SLIDE_TRANSITION_TIME_MS = 4000;
+const SLIDE_STOP_TIME_MS = 5000;
 
 $(window).on('load', () => {
 
     // Initialize
     const slider = $('.slider__animals');
     const slides = slider.children('.animal-slide');
-    slides[0].classList.add('left');
-    slides[1].classList.add('center');
-    slides[2].classList.add('right');
-    let leftSlideIndex = 0;
-    let centerSlideIndex = 1;
-    let rightSlideIndex = 2;
+    let left = 0;
+    slides[left + 0].classList.add('left');
+    slides[left + 1].classList.add('center');
+    slides[left + 2].classList.add('right');
+    slides[left + 3].classList.add('right-2');
+    function nextSlide() {
+        slides.addClass('transition');
+        slides[(slides.length + left - 1) % slides.length].classList.remove('left-2')
+        slides[(left + 0) % slides.length].classList.remove('left');
+        slides[(left + 1) % slides.length].classList.remove('center');
+        slides[(left + 2) % slides.length].classList.remove('right');
+        slides[(left + 3) % slides.length].classList.remove('right-2');
+        left = (left + 1) % slides.length;
+        slides[(slides.length + left - 1) % slides.length].classList.add('left-2')
+        slides[(left + 0) % slides.length].classList.add('left');
+        slides[(left + 1) % slides.length].classList.add('center');
+        slides[(left + 2) % slides.length].classList.add('right');
+        slides[(left + 3) % slides.length].classList.add('right-2');
+    }
 
     // Slide loop
-    setInterval(() => {
-        slides[leftSlideIndex].classList.remove('left');
-        slides[centerSlideIndex].classList.remove('center');
-        slides[rightSlideIndex].classList.remove('right');
-        leftSlideIndex = (leftSlideIndex + 1) % slides.length;
-        centerSlideIndex = (centerSlideIndex + 1) % slides.length;
-        rightSlideIndex = (rightSlideIndex + 1) % slides.length;
-        slides[leftSlideIndex].classList.add('left');
-        slides[centerSlideIndex].classList.add('center');
-        slides[rightSlideIndex].classList.add('right');
-        const zeroSlide = slides[0];
-        zeroSlide.remove();
-        slider.append(zeroSlide);
-    }, SLIDE_STOP_TIME_MS);
+    setInterval(nextSlide, SLIDE_STOP_TIME_MS);
+    setTimeout(nextSlide, SLIDE_STOP_TIME_MS - SLIDE_TRANSITION_TIME_MS);
 });
